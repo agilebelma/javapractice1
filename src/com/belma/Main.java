688d05c9f13a71271e34774f55454251a2c8d855
Belma Gaukrodger <AgileBelma@gmail.com>
2019-04-07 16:47:41 +1200

    add myNumber

--- src/com/belma/Main.java
+++ src/com/belma/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         char myChar = '\u00AE';
+        int myNumber = 8;
         System.out.println("Unicode output was: " + myChar);
     }
 }
